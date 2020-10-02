d02103e516564ac60270fa880994bc2ef29dae73
vinnidias <vinicius.3991@gmail.com>
2020-10-02 18:28:27 -0300

    style file

--- gamingEcommerce/src/styles.js
+++ gamingEcommerce/src/styles.js
@@ -1,7 +1,32 @@
 const styles = {
     productCardContainer: {
-        width: 300,
-        height: 500,
-        borderWidth: 1
+        width: 250,
+        height: 350,
+        margin: 10,
+        padding: 10,
+        borderWidth: 1,
+        alignItems: 'center',
+        alignSelf: 'center'
+    },
+
+    shopScreenContainer: {
+        flex: 1
+    },
+
+    shopScreenHeader: {
+        flex: 1,
+        height: 80,
+        backgroundColor: '#1e90ff'
+    },
+
+    cartHeader: {
+        alignSelf: 'flex-end',
+        margin: 20
+    },
+    
+    buttonCardContainer: {
+        flexDirection: 'row'
     }
 }
+
+export default styles
