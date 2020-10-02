09c574649e509a23848fec44764d8b09eafe47e8
vinnidias <vinicius.3991@gmail.com>
2020-10-02 15:58:25 -0300

    starting the shop and cart screens

---
+++ gamingEcommerce/src/screens/CartScreen.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { View, Text } from 'react-native'
+
+const ShopScreen = () => {
+	return (
+		<View>
+			<Text>
+				Tela do carrinho
+		</Text>
+		</View>
+	)
+}
+
+export default ShopScreen
---
+++ gamingEcommerce/src/screens/ShopScreen.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { View, Text } from 'react-native'
+import productsList from '../products/products.json'
+
+
+const ShopScreen = () => {
+	return (
+		
+	)
+}
+
+export default ShopScreen
