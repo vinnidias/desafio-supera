0d93c4fc8c7c9c90566bde23725fac78c6ae3917
vinnidias <vinicius.3991@gmail.com>
2020-10-02 18:31:06 -0300

    first steps on shop screen component

--- gamingEcommerce/src/screens/ShopScreen.js
+++ gamingEcommerce/src/screens/ShopScreen.js
@@ -1,11 +1,25 @@
 import React from "react"
 import { View, Text } from 'react-native'
-import productsList from '../products/products.json'
+import productsArray from '../products/products.json'
+import ProductsList from "../components/ProductsList"
+import styles from "../styles"
+import CartButton from '../components/CartButton'
 
 
 const ShopScreen = () => {
 	return (
-		
+		<View style={styles.shopScreenContainer}>
+			<View style={styles.shopScreenHeader}>
+				<View style={styles.cartHeader}>
+					<CartButton />
+				</View>
+			</View>
+			<View style={{flex: 8}}>
+				<ProductsList array={productsArray} />
+
+			</View>
+		</View>
+
 	)
 }
 
