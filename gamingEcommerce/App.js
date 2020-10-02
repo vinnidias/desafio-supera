3d16f3bee364a6ad6462b0811bceff60c6b65574
vinnidias <vinicius.3991@gmail.com>
2020-10-02 15:00:35 -0300

    react native cli setup

---
+++ gamingEcommerce/App.js
@@ -0,0 +1,22 @@
+
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return(
+    <View>
+      <Text>
+        gaming e-comerce
+      </Text>
+    </View>
+  )
+}
+export default App;
