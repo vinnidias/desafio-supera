b50c0ee1b3039c277303081a328f49685778c701
vinnidias <vinicius.3991@gmail.com>
2020-10-03 14:45:33 -0300

    product quantity component

---
+++ gamingEcommerce/src/components/QuantityButton.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { TouchableOpacity, Image, View, Text } from 'react-native'
+import styles from '../styles'
+
+const QuantityButton = () => {
+	const [quantity, setQuantity] = useState(1)
+	return (
+		<View style={styles.quantityButton}>
+			<TouchableOpacity onPress={() => {
+				if (quantity <= 1) {
+					setQuantity(1)
+				} else { setQuantity(quantity - 1) }
+			}} >
+				<View style={styles.minusButton}>
+					<Text style={{ alignSelf: 'center' }}>
+						➖
+					</Text>
+				</View>
+			</TouchableOpacity>
+			<Text style={styles.quantityText}>
+				Qtd: {quantity}
+			</Text>
+			<TouchableOpacity onPress={() => setQuantity(quantity + 1)} >
+				<View style={styles.plusButton}>
+					<Text style={{ alignSelf: 'center' }}>
+						➕
+					</Text>
+				</View>
+			</TouchableOpacity>
+		</View>
+	)
+}
+
+export default QuantityButton
