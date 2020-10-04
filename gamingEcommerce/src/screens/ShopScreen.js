d0b93c0a5c1f90fdc668a30b72d67e31560ded1b
vinnidias <vinicius.3991@gmail.com>
2020-10-04 18:30:42 -0300

    creating the functionality for the shop screen

--- gamingEcommerce/src/screens/ShopScreen.js
+++ gamingEcommerce/src/screens/ShopScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React, { useState } from "react"
 import { View, Text } from 'react-native'
 import products from '../products/products'
 import ProductsList from "../components/ProductsList"
@@ -6,16 +6,22 @@ import styles from "../styles"
 import CartButton from '../components/CartButton'
 
 
-const ShopScreen = () => {
+const ShopScreen = ({navigation}) => {
+	const cart = []
+	const [cartItems, setCartItems] = useState(0)
 	return (
 		<View style={styles.shopScreenContainer}>
 			<View style={styles.shopScreenHeader}>
 				<View style={styles.cartHeader}>
-					<CartButton />
+					<CartButton onPress={()=> navigation.navigate('CartScreen', {cart})}/>
+					
 				</View>
 			</View>
 			<View style={{flex: 8}}>
-				<ProductsList array={products} />
+				<ProductsList array={products} onPress={(id, name, price, score)=>{ 
+					cart.push({id, name, price, score})
+					console.log(cart)
+				}}/>
 
 			</View>
 		</View>
