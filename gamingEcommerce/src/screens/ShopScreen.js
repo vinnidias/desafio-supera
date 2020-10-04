4b72c1b1fd6881b1380368df3ab8080ec170c322
vinnidias <vinicius.3991@gmail.com>
2020-10-04 19:04:44 -0300

    adding  toast android to confirm addition to cart

--- gamingEcommerce/src/screens/ShopScreen.js
+++ gamingEcommerce/src/screens/ShopScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { View, Text } from 'react-native'
+import { View, Text, ToastAndroid } from 'react-native'
 import products from '../products/products'
 import ProductsList from "../components/ProductsList"
 import styles from "../styles"
@@ -8,19 +8,18 @@ import CartButton from '../components/CartButton'
 
 const ShopScreen = ({navigation}) => {
 	const cart = []
-	const [cartItems, setCartItems] = useState(0)
 	return (
 		<View style={styles.shopScreenContainer}>
 			<View style={styles.shopScreenHeader}>
 				<View style={styles.cartHeader}>
 					<CartButton onPress={()=> navigation.navigate('CartScreen', {cart})}/>
-					
 				</View>
 			</View>
 			<View style={{flex: 8}}>
-				<ProductsList array={products} onPress={(id, name, price, score)=>{ 
+				<ProductsList array={products} onPress={async (id, name, price, score)=>{ 
 					cart.push({id, name, price, score})
 					console.log(cart)
+					ToastAndroid.show("Produto adicionado ao carrinho!", ToastAndroid.SHORT)
 				}}/>
 
 			</View>
