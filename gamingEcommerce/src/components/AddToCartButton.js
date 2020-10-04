b9371a662b401e59889048732a18210fde372a5d
vinnidias <vinicius.3991@gmail.com>
2020-10-04 18:31:03 -0300

    spell check

--- gamingEcommerce/src/components/AddToCartButton.js
+++ gamingEcommerce/src/components/AddToCartButton.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react'
 import { TouchableOpacity, Image, View, Text } from 'react-native'
-import { exp } from 'react-native/Libraries/Animated/src/Easing'
 import styles from '../styles'
 
 const AddToCarButton = ({onPress}) => {
     return (
         <TouchableOpacity onPress={onPress}>
-            <View style={styles.addToCartContaine}>
+            <View style={styles.addToCartContainer}>
                 <Image source={require('../assets/cart-icon.png')} style={styles.addToCartIcon}/>
                 <Text style={styles.addToCartText}>
                     Mandar pro carrinho
